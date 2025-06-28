5695a0a8b808563f30b63adb42a9db3cea095998
Ahmed Tarek <ahmedtarek7580@gmail.com>
2025-06-28 17:47:15 +0300

    order Detial Action

--- Bulky_Web/Areas/Admin/Controllers/OrderController.cs
+++ Bulky_Web/Areas/Admin/Controllers/OrderController.cs
@@ -1,6 +1,7 @@
 ﻿using BulkyBook.DataAccess.IRepository;
 using BulkyBook.DataAccess.Repository;
 using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
 using BulkyBook.Utility;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,27 +20,37 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
         {
             return View();
         }
+        public IActionResult Details(int orderId)
+        {
+            OrderVM orderVM = new OrderVM()
+            {
+                orderHeader = unitOfWork.Header.Get(u => u.Id == orderId, includeProperties: "ApplicationUser"),
+                orderDetail = [.. unitOfWork.Details.GetAll(u => u.OrderHeaderId == orderId, includeProperties: "Product")]
+            };
+            return View(orderVM);
+        }
 
         #region API Call
         [HttpGet]
         public IActionResult GetAll(string status)
         {
             List<OrderHeader> objHeaderList = unitOfWork.Header.GetAll(includeProperties: "ApplicationUser").ToList();
-            switch (status) {
+            switch (status)
+            {
                 case "pending":
-                objHeaderList = objHeaderList.Where(u => u.PaymentStatus == SD.PaymentStatusDelayedPayment|| u.PaymentStatus == SD.PaymentStatusPending).ToList();
-                break;
-            case "inprocess":
-                objHeaderList = objHeaderList.Where(u => u.OrderStatus == SD.StatusInProcess).ToList();
-                break;
-            case "completed":
-                objHeaderList = objHeaderList.Where(u => u.OrderStatus == SD.StatusShipped).ToList();
-                break;
-            case "approved":
-                objHeaderList = objHeaderList.Where(u => u.OrderStatus == SD.StatusApproved).ToList();
-                break;
-            default:
-                break;
+                    objHeaderList = objHeaderList.Where(u => u.PaymentStatus == SD.PaymentStatusDelayedPayment || u.PaymentStatus == SD.PaymentStatusPending).ToList();
+                    break;
+                case "inprocess":
+                    objHeaderList = objHeaderList.Where(u => u.OrderStatus == SD.StatusInProcess).ToList();
+                    break;
+                case "completed":
+                    objHeaderList = objHeaderList.Where(u => u.OrderStatus == SD.StatusShipped).ToList();
+                    break;
+                case "approved":
+                    objHeaderList = objHeaderList.Where(u => u.OrderStatus == SD.StatusApproved).ToList();
+                    break;
+                default:
+                    break;
 
             }
             return Json(new { data = objHeaderList });
