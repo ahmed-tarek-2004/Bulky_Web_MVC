d7ea20871865c91d1840e7a5e016452836ada638
Ahmed Tarek <ahmedtarek7580@gmail.com>
2025-06-30 05:30:32 +0300

    DBInitializer

--- Bulky.DataAccess/obj/Debug/net9.0/BulkyBook.DataAccess.AssemblyInfo.cs
+++ Bulky.DataAccess/obj/Debug/net9.0/BulkyBook.DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyBook.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b7cbde1c221078ca438832ab078e046f4ffbbce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12c222103c501467c5926c7530909728796c7878")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyBook.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyBook.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
