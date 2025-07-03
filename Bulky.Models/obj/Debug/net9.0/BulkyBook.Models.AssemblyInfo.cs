48180a6f05ecab97b4d9f20a0c25ba9ad66dc323
Ahmed Tarek <ahmedtarek7580@gmail.com>
2025-07-03 04:35:54 +0300

    Edit User Role

--- Bulky.Models/obj/Debug/net9.0/BulkyBook.Models.AssemblyInfo.cs
+++ Bulky.Models/obj/Debug/net9.0/BulkyBook.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyBook.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fb25f6a702d1559b25834963b507de3aba2df6b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9bffef5e84a00c7db628b07f437cebe304ab2d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyBook.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyBook.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Bulky_Web/Areas/Admin/Controllers/UserController.cs
+++ Bulky_Web/Areas/Admin/Controllers/UserController.cs
@@ -1,9 +1,12 @@
 ﻿using BulkyBook.DataAccess.Data;
 using BulkyBook.DataAccess.IRepository;
 using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
 using BulkyBook.Utility;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace BulkyBookWeb.Areas.Admin.Controllers
@@ -14,9 +17,11 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
     {
 
         private readonly ApplicationDbContext context;
-        public UserController(ApplicationDbContext _context)
+        private readonly UserManager<IdentityUser> userManager;
+        public UserController(ApplicationDbContext _context, UserManager<IdentityUser> userManager)
         {
             context = _context;
+            this.userManager = userManager;
         }
         public IActionResult Index()
         {
@@ -37,7 +42,57 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
             context.SaveChanges();
             return Json(new { success = true, message = "Operation Successful" });
         }
+        public IActionResult RoleManagment(string userId)
+        {
 
+            string RoleID = context.UserRoles.FirstOrDefault(u => u.UserId == userId).RoleId;
+
+            RoleManagmentVM RoleVM = new RoleManagmentVM()
+            {
+                ApplicationUser = context.ApplicationUsers.Include(u => u.Company).FirstOrDefault(u => u.Id == userId),
+                RoleList = context.Roles.Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Name
+                }).ToList(),
+                CompanyList = context.Companies.Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                }).ToList()
+            };
+
+            RoleVM.ApplicationUser.Role = context.Roles.FirstOrDefault(u => u.Id == RoleID).Name;
+            return View(RoleVM);
+        }
+        [HttpPost]
+        public IActionResult RoleManagment(RoleManagmentVM roleManagmentVM)
+        {
+
+            string RoleID = context.UserRoles.FirstOrDefault(u => u.UserId == roleManagmentVM.ApplicationUser.Id).RoleId;
+            string oldRole = context.Roles.FirstOrDefault(u => u.Id == RoleID).Name;
+
+            if (!(roleManagmentVM.ApplicationUser.Role == oldRole))
+            {
+                //a role was updated
+                ApplicationUser applicationUser = context.ApplicationUsers.FirstOrDefault(u => u.Id == roleManagmentVM.ApplicationUser.Id);
+                if (roleManagmentVM.ApplicationUser.Role == SD.Role_Company)
+                {
+                    applicationUser.CompanyId = roleManagmentVM.ApplicationUser.CompanyId;
+                }
+                if (oldRole == SD.Role_Company)
+                {
+                    applicationUser.CompanyId = null;
+                }
+                context.SaveChanges();
+
+                userManager.RemoveFromRoleAsync(applicationUser, oldRole).GetAwaiter().GetResult();
+                userManager.AddToRoleAsync(applicationUser, roleManagmentVM.ApplicationUser.Role).GetAwaiter().GetResult();
+
+            }
+
+            return RedirectToAction("Index");
+        }
         #region API CALLS
         [HttpGet]
         public IActionResult GetAll()
@@ -61,7 +116,5 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
             return Json(new { data = objUserList });
         }
         #endregion
-
-
     }
 }
