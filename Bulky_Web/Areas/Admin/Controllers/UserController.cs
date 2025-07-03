23ab40208f36441b13ea86386e6e084cbfa153af
Ahmed Tarek <ahmedtarek7580@gmail.com>
2025-07-03 20:30:40 +0300

    apply update for user identity

--- Bulky_Web/Areas/Admin/Controllers/UserController.cs
+++ Bulky_Web/Areas/Admin/Controllers/UserController.cs
@@ -1,5 +1,4 @@
-﻿using BulkyBook.DataAccess.Data;
-using BulkyBook.DataAccess.IRepository;
+﻿using BulkyBook.DataAccess.IRepository;
 using BulkyBook.Models;
 using BulkyBook.Models.ViewModels;
 using BulkyBook.Utility;
@@ -8,6 +7,8 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Data;
 
 namespace BulkyBookWeb.Areas.Admin.Controllers
 {
@@ -15,66 +16,56 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
     [Authorize(Roles = SD.Role_Admin)]
     public class UserController : Controller
     {
-
-        private readonly ApplicationDbContext context;
-        private readonly UserManager<IdentityUser> userManager;
-        public UserController(ApplicationDbContext _context, UserManager<IdentityUser> userManager)
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IUnitOfWork _unitOfWork;
+        public UserController(UserManager<IdentityUser> userManager, IUnitOfWork unitOfWork, RoleManager<IdentityRole> roleManager)
         {
-            context = _context;
-            this.userManager = userManager;
+            _unitOfWork = unitOfWork;
+            _roleManager = roleManager;
+            _userManager = userManager;
         }
         public IActionResult Index()
         {
-            return View("Index");
+            return View();
         }
 
-
-        [HttpPost]
-        public IActionResult LockUnlock([FromBody] string? Id)
-        {
-            var user = context.ApplicationUsers.FirstOrDefault(b => b.Id == Id);
-            if (user == null)
-            {
-                return Json(new { success = false, message = "Error while Locking/Unlocking" });
-            }
-            user.LockoutEnd = (user.LockoutEnd != null && user.LockoutEnd > DateTime.Now) ? DateTime.Now 
-                : DateTime.Now.AddYears(100);
-            context.SaveChanges();
-            return Json(new { success = true, message = "Operation Successful" });
-        }
         public IActionResult RoleManagment(string userId)
         {
 
-            string RoleID = context.UserRoles.FirstOrDefault(u => u.UserId == userId).RoleId;
-
             RoleManagmentVM RoleVM = new RoleManagmentVM()
             {
-                ApplicationUser = context.ApplicationUsers.Include(u => u.Company).FirstOrDefault(u => u.Id == userId),
-                RoleList = context.Roles.Select(i => new SelectListItem
+                ApplicationUser = _unitOfWork.ApplicationUser.Get(u => u.Id == userId, includeProperties: "Company"),
+                RoleList = _roleManager.Roles.Select(i => new SelectListItem
                 {
                     Text = i.Name,
                     Value = i.Name
                 }).ToList(),
-                CompanyList = context.Companies.Select(i => new SelectListItem
+                CompanyList = _unitOfWork.Company.GetAll().Select(i => new SelectListItem
                 {
                     Text = i.Name,
                     Value = i.Id.ToString()
                 }).ToList()
             };
 
-            RoleVM.ApplicationUser.Role = context.Roles.FirstOrDefault(u => u.Id == RoleID).Name;
+            RoleVM.ApplicationUser.Role = _userManager.GetRolesAsync(_unitOfWork.ApplicationUser.Get(u => u.Id == userId))
+                    .GetAwaiter().GetResult().FirstOrDefault();
             return View(RoleVM);
         }
+
         [HttpPost]
         public IActionResult RoleManagment(RoleManagmentVM roleManagmentVM)
         {
 
-            string RoleID = context.UserRoles.FirstOrDefault(u => u.UserId == roleManagmentVM.ApplicationUser.Id).RoleId;
-            string oldRole = context.Roles.FirstOrDefault(u => u.Id == RoleID).Name;
+            string oldRole = _userManager.GetRolesAsync(_unitOfWork.ApplicationUser.Get(u => u.Id == roleManagmentVM.ApplicationUser.Id))
+                    .GetAwaiter().GetResult().FirstOrDefault();
+
+            ApplicationUser applicationUser = _unitOfWork.ApplicationUser.Get(u => u.Id == roleManagmentVM.ApplicationUser.Id);
+
 
             if (!(roleManagmentVM.ApplicationUser.Role == oldRole))
             {
-                ApplicationUser applicationUser = context.ApplicationUsers.FirstOrDefault(u => u.Id == roleManagmentVM.ApplicationUser.Id);
+                //a role was updated
                 if (roleManagmentVM.ApplicationUser.Role == SD.Role_Company)
                 {
                     applicationUser.CompanyId = roleManagmentVM.ApplicationUser.CompanyId;
@@ -83,28 +74,40 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
                 {
                     applicationUser.CompanyId = null;
                 }
-                context.SaveChanges();
+                _unitOfWork.ApplicationUser.Update(applicationUser);
+                _unitOfWork.Save();
 
-                userManager.RemoveFromRoleAsync(applicationUser, oldRole).GetAwaiter().GetResult();
-                userManager.AddToRoleAsync(applicationUser, roleManagmentVM.ApplicationUser.Role).GetAwaiter().GetResult();
+                _userManager.RemoveFromRoleAsync(applicationUser, oldRole).GetAwaiter().GetResult();
+                _userManager.AddToRoleAsync(applicationUser, roleManagmentVM.ApplicationUser.Role).GetAwaiter().GetResult();
 
+            }
+            else
+            {
+                if (oldRole == SD.Role_Company && applicationUser.CompanyId != roleManagmentVM.ApplicationUser.CompanyId)
+                {
+                    applicationUser.CompanyId = roleManagmentVM.ApplicationUser.CompanyId;
+                    _unitOfWork.ApplicationUser.Update(applicationUser);
+                    _unitOfWork.Save();
+                }
             }
 
             return RedirectToAction("Index");
         }
+
+
         #region API CALLS
+
         [HttpGet]
         public IActionResult GetAll()
         {
-            List<ApplicationUser> objUserList = context.ApplicationUsers.Include(u => u.Company).ToList();
-            var userRole = context.UserRoles.ToList();
-            var roles = context.Roles.ToList();
+            List<ApplicationUser> objUserList = _unitOfWork.ApplicationUser.GetAll(includeProperties: "Company").ToList();
+
             foreach (var user in objUserList)
             {
-                var uRoles = userRole.FirstOrDefault(u => u.UserId == user.Id);
-                var role = roles.FirstOrDefault(u => u.Id == uRoles.RoleId);
-                user.Role = role.Name;
-                if (user is null)
+
+                user.Role = _userManager.GetRolesAsync(user).GetAwaiter().GetResult().FirstOrDefault();
+
+                if (user.Company == null)
                 {
                     user.Company = new Company()
                     {
@@ -112,8 +115,35 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
                     };
                 }
             }
+
             return Json(new { data = objUserList });
         }
+
+
+        [HttpPost]
+        public IActionResult LockUnlock([FromBody] string id)
+        {
+
+            var objFromDb = _unitOfWork.ApplicationUser.Get(u => u.Id == id);
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while Locking/Unlocking" });
+            }
+
+            if (objFromDb.LockoutEnd != null && objFromDb.LockoutEnd > DateTime.Now)
+            {
+                //user is currently locked and we need to unlock them
+                objFromDb.LockoutEnd = DateTime.Now;
+            }
+            else
+            {
+                objFromDb.LockoutEnd = DateTime.Now.AddYears(1000);
+            }
+            _unitOfWork.ApplicationUser.Update(objFromDb);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Operation Successful" });
+        }
+
         #endregion
     }
 }
