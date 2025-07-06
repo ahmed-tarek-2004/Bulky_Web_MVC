905cb184255a5dcdd98a0b92119f163704b59ab3
Ahmed Tarek Zaher <122125653+ahmed-tarek-2004@users.noreply.github.com>
2025-07-06 20:20:07 +0300

    Update ApplicationDbContext.cs

--- Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -7,7 +7,7 @@ namespace BulkyBook.DataAccess.Data
 {
     public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
-        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
